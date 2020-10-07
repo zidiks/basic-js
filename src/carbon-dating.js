7f8eee0fa23182bc55083fe2c2ff37a5afcfd0f0
Mironov Vladimir <zidiks228@gmail.com>
2020-10-07 17:05:33 +0300

    upd: save solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+    const resp = Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
+    return parseInt(sampleActivity) && typeof sampleActivity == 'string' && resp > 0 ? resp : false;
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(arr) {
+    return arr.reduce((sum, item) => sum += item.reduce((sum, item) => sum += item == '^^' ? 1 : 0, 0), 0);
 };

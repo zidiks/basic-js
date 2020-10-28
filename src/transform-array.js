5f6125a9e4fd3c4960ee63c6882a1d488410509e
Mironov Vladimir <zidiks228@gmail.com>
2020-10-28 17:58:34 +0300

    upd: add transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform( /* arr */ ) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    let discardNext = false;
+    return arr.reduce((newarr, item, index, array) => {
+        switch (item) {
+            case '--discard-next':
+                if (array[index + 1]) discardNext = true;
+                newarr.push('dell');
+                return newarr;
+            case '--discard-prev':
+                if (array[index - 1]) newarr[newarr.length - 2] = 'dell';
+                newarr.push('dell');
+                return newarr;
+            case '--double-next':
+                if (array[index + 1]) newarr.push(array[index + 1]);
+                return newarr;
+            case '--double-prev':
+                if (array[index - 1]) newarr.push(newarr[newarr.length - 2]);
+                return newarr;
+            default:
+                if (discardNext) {
+                    discardNext = false;
+                    newarr.push('dell');
+                } else newarr.push(item);
+                return newarr;
+        }
+    }, []).filter(function(x) {
+        return x !== 'dell';
+    });
 };

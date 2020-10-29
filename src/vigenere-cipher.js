4db84a5da63a656fa9bd500cf38bd8a967c8e826
Mironov Vladimir <zidiks228@gmail.com>
2020-10-29 13:09:59 +0300

    upd: add vigenere chiper

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(mode = true) {
+        this.mode = mode;
+        this.alphabeet = [...
+            'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        ];
+    }
+    rev(str) {
+        if (this.mode) return str;
+        else return str.split('').reverse().join('');
+    }
+    encrypt(message, key) {
+        let count = 0;
+        key = key.toUpperCase().split('');
+        return this.rev(message.toUpperCase().split('').reduce((res, item, index) => {
+            if (this.alphabeet.indexOf(item) == -1) {
+                res += item;
+            } else {
+                let x = this.alphabeet.indexOf(item) + this.alphabeet.indexOf(key[count]);
+                count = count + 1 > key.length - 1 ? 0 : count + 1;
+                res += (this.alphabeet[x > 25 ? x - 26 : x]);
+            }
+            return res;
+        }, ''));
+    }
+    decrypt(message, key) {
+        let count = 0;
+        key = key.toUpperCase().split('');
+        return this.rev(message.toUpperCase().split('').reduce((res, item, index) => {
+            if (this.alphabeet.indexOf(item) == -1) {
+                res += item;
+            } else {
+                let x = this.alphabeet.indexOf(item) - this.alphabeet.indexOf(key[count]);
+                count = count + 1 > key.length - 1 ? 0 : count + 1;
+                res += (this.alphabeet[x < 0 ? x + 26 : x]);
+            }
+            return res;
+        }, ''));
+    }
 }
 
 module.exports = VigenereCipheringMachine;

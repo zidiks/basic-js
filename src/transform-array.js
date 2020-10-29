a63d6463dccb8360b2f965edf2d620766e93d1bc
Mironov Vladimir <zidiks228@gmail.com>
2020-10-29 03:03:12 +0300

    upd: add transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,31 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-    let discardNext = false;
-    return arr.reduce((newarr, item, index, array) => {
-        switch (item) {
-            case '--discard-next':
-                if (array[index + 1]) discardNext = true;
-                newarr.push('dell');
-                return newarr;
-            case '--discard-prev':
-                if (array[index - 1]) newarr[newarr.length - 2] = 'dell';
-                newarr.push('dell');
-                return newarr;
-            case '--double-next':
-                if (array[index + 1]) newarr.push(array[index + 1]);
-                return newarr;
-            case '--double-prev':
-                if (array[index - 1]) newarr.push(newarr[newarr.length - 2]);
-                return newarr;
-            default:
-                if (discardNext) {
-                    discardNext = false;
-                    newarr.push('dell');
-                } else newarr.push(item);
-                return newarr;
-        }
-    }, []).filter(function(x) {
-        return x !== 'dell';
-    });
+    let newArr = [];
+    if (Array.isArray(arr)) {
+        if (arr.length !== 0) {
+            for (let index = 0; index < arr.length; index++) {
+                switch (arr[index]) {
+                    case '--discard-next':
+                        if (arr[index + 2] == '--double-prev' || arr[index + 2] == '--discard-prev') {
+                            index += 2;
+                        } else index++;
+                        break;
+                    case '--discard-prev':
+                        if (index > 0) newArr.pop();
+                        break;
+                    case '--double-next':
+                        if (index < arr.length - 1) newArr.push(arr[index + 1]);
+                        break;
+                    case '--double-prev':
+                        if (index > 0) newArr.push(arr[index - 1]);
+                        break;
+                    default:
+                        newArr.push(arr[index]);
+                        break
+                }
+            }
+        } else return arr;
+    } else throw 'THROWN';
+    return newArr;
 };
